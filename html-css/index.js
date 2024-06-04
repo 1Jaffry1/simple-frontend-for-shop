bbdf1d225b7ab50e08552678463214724426775d
Sayyed Muhammad Jaffry <muhammadj110@gmail.com>
2024-06-04 17:35:27 +0330

    changed naming convention for images

--- html-css/index.js
+++ html-css/index.js
@@ -1,6 +1,6 @@
 let products = document.getElementsByClassName("product");
 for (let i = 0; i < products.length; i++) {
-    products[i].setAttribute('name', "msh" + (i+1));
+    products[i].setAttribute('name', "p" + (i+1));
     setProduct(products[i], i+1);
     products[i].innerHTML += '<div><br><button class="add-to-cart">Add to Cart</button></div> '
     products[i].getElementsByTagName("span")[0].innerHTML = "$" + (i+1)*10*0.7 + ".99";
@@ -12,18 +12,9 @@ function setProduct(product, index) {
         window.location.href = document.location.parent + "/../product.html?name=" + product.getAttribute("name");
     }
 
-    // product.getElementsByTagName("h2")[0].innerHTML = "Product " + product.getAttribute("name")[product.getAttribute("name").length-3, product.getAttribute("name").length-1];
     product.getElementsByTagName("p")[0].innerHTML = "Discription of Product " + product.getAttribute("name");
     product.getElementsByTagName("h2")[0].innerHTML = product.getAttribute("name");
     product.getElementsByTagName("img")[0].setAttribute("src", "/images/shoes-photo/p"+index+"/p"+index+"-1.jpg");
     product.getElementsByTagName("img")[0].setAttribute("alt","shoe"+index);
 }
 
-// function createProduct(name, discription ,price, img) {
-//     document.getElementById("maingrid").innerHTML += `<div class="product" name="`+name+`">
-//     <img src="/images/shoes/`+img+`.jpg" alt="`+img+`">
-//     <span> $`+price+`</span>
-// </div>`;
-
-// return HTMLElement()
-// }

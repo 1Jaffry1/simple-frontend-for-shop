d66b97f823c39d207b783608b08b9ea935e70145
Sayyed Muhammad Jaffry <muhammadj110@gmail.com>
2024-06-04 11:43:50 +0330

    fixed product being too big, still centered last product

--- html-css/index.js
+++ html-css/index.js
@@ -3,6 +3,7 @@ for (let i = 0; i < products.length; i++) {
     products[i].setAttribute('name', "msh" + (i+1));
     setProduct(products[i]);
 }
+// setProduct(createProduct("msh10", "this is disc1", 12, "shoe4"));
 
 function setProduct(product, index, price) {
     product.onclick = function () {
@@ -13,3 +14,12 @@ function setProduct(product, index, price) {
     product.getElementsByTagName("p")[0].innerHTML = "Discription of Product " + product.getAttribute("name");
     product.getElementsByTagName("h2")[0].innerHTML = product.getAttribute("name");
 }
+
+// function createProduct(name, discription ,price, img) {
+//     document.getElementById("maingrid").innerHTML += `<div class="product" name="`+name+`">
+//     <img src="/images/shoes/`+img+`.jpg" alt="`+img+`">
+//     <span> $`+price+`</span>
+// </div>`;
+
+// return HTMLElement()
+// }
